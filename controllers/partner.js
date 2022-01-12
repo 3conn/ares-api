8d352fffe4880ffe485edb0c12c31c90b7f4757b
3conn <jp.tree.connection@gmail.com>
2022-01-11 22:49:09 -0300

    definições

--- controllers/partner.js
+++ controllers/partner.js
@@ -4,7 +4,7 @@ module.exports = () => {
   
     controller.list = (req, res) => res.status(200).json(DB);
   
-    controller.findbyid = (req, res, next) => res.status(200).json(DB, req.params.id);
+    controller.findbyid = (req, res, next) => res.status(200).send({message: "uiuiui"});
 
     return controller;
   }
